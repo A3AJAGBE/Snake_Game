c04b25a944052e5b139129940979cb90342110ea
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-05 08:10:45 +0000

    Create the snake

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.title("A3AJAGBE SNAKE GAME")
 
+# Create the snake
+posX = 0
+posY = 0
+initial_length = []
+for _ in range(3):
+    sample = turtle.Turtle("square")
+    sample.goto(x=posX, y=posY)
+    posX += -20
+    initial_length.append(sample)
+
 # Keep the screen open until closed
 screen.exitonclick()
