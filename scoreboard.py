1810804ebf4a9e1a18fb6d848f4881370d945a94
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-05 10:30:09 +0000

    Add scoreboard class and functions

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Arial", 20, "bold")
+ALIGN = "center"
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(x=0, y=275)
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        """This function increases the score"""
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        """This function updates the scoreboard"""
+        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
