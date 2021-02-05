313a48529aff9a479f596a644485579eac501619
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-05 07:53:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Import
+import turtle
+
+# Instantiate Screen
+screen = turtle.Screen()
+
+# Set up the screen
+screen.setup(width=600, height=600)
+screen.title("A3AJAGBE SNAKE GAME")
+
+# Keep the screen open until closed
+screen.exitonclick()
